be69ca62a0df12390a88eb52d2a662b466ec0efb
tebii <105075744+tebii@users.noreply.github.com>
2024-08-06 23:59:29 +0800

    Add main.py

--- main.py
+++ main.py
@@ -1,1 +1,63 @@
+import requests
+from datetime import datetime
+import os
+
+TOKEN = os.environ.get("PIXLA_TOKEN")
+USERNAME= os.environ.get("PIXLA_USERNAME")
+GRAPH_ID= "graph1"
+pixela_endpoint = "https://pixe.la/v1/users"
+
+user_params = {
+    "token": TOKEN,
+    "username": USERNAME, 
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes",
+}
+
+# response = requests.post(url=pixela_endpoint, json=user_params)
+# print(response.text)
+
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+
+graph_config = {
+    "id": GRAPH_ID,
+    "name": "Cycling Graph",
+    "unit": "km",
+    "type": "float",
+    "color": "ajisai"
+}
+
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+
+# response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+# print(response.text)
+
+pixel_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
+
+today = datetime.today()
+today = (today.strftime("%Y%m%d"))
+
+pixel_config = {
+    "date": today,
+    "quantity": input("How many kilometers did you cycle today? "),
+}
+
+response = requests.post(url=pixel_endpoint, json=pixel_config, headers=headers)
+print(response.text)
+
+update_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/20240806"
+
+new_pixel_data = {
+    "quantity": "4.5"
+}
+
+# response = requests.put(url=update_endpoint, json=new_pixel_data, headers=headers)
+# print(response.text)
+
+delete_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/20240806"
+
+# response = requests.delete(url=delete_endpoint, headers=headers)
+# print(response.text)
 
